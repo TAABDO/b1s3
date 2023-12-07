041c54b712c2c8ddf71baec0c679737f44b8fae3
Aabilla Said <said160aabilla@gmail.com>
2023-12-07 10:54:56 +0100

    first commit

---
+++ database/connection.php
@@ -0,0 +1,21 @@
+<?php
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
+
+// Database configuration
+$servername = $_ENV['DB_HOST'];
+$username = $_ENV['DB_USER'];
+$password = $_ENV['DB_PASSWORD'];
+$dbname = $_ENV['DB_NAME'];
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+?>
---
+++ views/layouts/header.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <title>Mise en situation</title>
+</head>
+<body>
+    <!-- Navigation Bar -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">My App</a>
+        
+        <!-- Navigation Links -->
+        <ul class="navbar-nav ml-auto">
+            <!-- Check if the user is authenticated -->
+            <?php
+            // Replace the following condition with your authentication check logic
+            //$isAuthenticated = false;
+
+           // if ($isAuthenticated) {
+                // Display Logout link if authenticated
+               // echo '<li class="nav-item"><a class="nav-link" href="#">Logout</a></li>';
+            //} else {
+                // Display Login and Register links if not authenticated
+                //echo '<li class="nav-item"><a class="nav-link" href="#">Login</a></li>';
+                //echo '<li class="nav-item"><a class="nav-link" href="#">Register</a></li>';
+           // }
+            ?>
+        </ul>
+    </nav>
+
+    <div class="container mt-4">
